9fc8d06bcbfe5f9d972c7655c8e61ac9fc69f803
Matt Gaunt <matt@bigblast.co>
2013-01-07 15:53:45 +0000

    Bigblast

---
+++ clients/bigblast/assets/js/animations.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function() {
+    
+    /* **** some super fancy animations? **** */
+    $('header #navigation').delay(400).animate({
+         'opacity' : '1',
+         'margin-top' : '+=30px'
+    }, { duration: 700, easing: 'swing' });
+	
+	$('#headline').delay(1100).animate({
+         'opacity' : '1'
+    }, { duration: 600, easing: 'swing' });
+    
+    $('#browser').delay(1100).animate({
+         'opacity' : '1'
+    }, { duration: 600, easing: 'swing' });
+    
+});
